5302b68b3cc1a8bdf3f3b7fa43be48c798fe0c10
MalikaHEDDAJI03 <Malika.Heddaji@emsi-edu.ma>
2025-11-18 22:48:47 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/ksoap2-android-releases/")
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/ksoap2-android-releases/")    }
+}
+
+rootProject.name = "Client Soap Android Kotlin avec KSOAP"
+include(":app")
